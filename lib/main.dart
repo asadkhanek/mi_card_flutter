d50d3b458d2227ee29051a6069a48956ac1448c3
masadkhanek <masadkhanek@gmail.com>
2021-01-18 23:57:31 +0500

    wight
    Signed-off-by: masadkhanek <masadkhanek@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.blueGrey,
         body: Container(),
       ),
     ),
