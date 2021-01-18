bcbddb93c1957b249c52be19623e0b8ebb5220fb
masadkhanek <masadkhanek@gmail.com>
2021-01-19 00:26:12 +0500

    position change
    Signed-off-by: masadkhanek <masadkhanek@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white10,
-        body: Container(),
-      ),
-    ),
+
   );
 }
 class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white10,
+        body: Container(),
+      ),
+    );;
   }
 }
