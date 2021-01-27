f020895a286f7413caa66bdd373e9d14dc32b6cf
masadkhanek <masadkhanek@gmail.com>
2021-01-27 22:48:12 +0500

    position change
    Signed-off-by: masadkhanek <masadkhanek@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.navyblue,
+        backgroundColor: Colors.greenAccent,
         body: Container(),
       ),
     ),
